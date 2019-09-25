17834ba1c3a5d3a956203c6ac530f07072b218a4
lcq2 <42534164+lcq2@users.noreply.github.com>
2019-09-25 20:59:38 +0200

    cleanup; rom is taken from command line;

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,43 @@ mod nes;
 use std::io::prelude::*;
 use std::fs;
 use std::time;
+use std::env;
+use std::error::Error;
 
-extern crate sdl2;
 use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::surface;
-use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
-fn main() {
-    let mut nes = nes::Nes::new("roms/super_mario_bros.nes").unwrap();
-    let mut elasped: Option<time::Instant> = None;
-    let test_status: u8 = 0xFF;
-    nes.powerup();
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
 
-    let sdl_ctx = sdl2::init().unwrap();
-    let video = sdl_ctx.video().unwrap();
+    let mut nes = nes::Nes::new(&args[1])?;
+
+    let sdl_ctx = sdl2::init()?;
+    let video = sdl_ctx.video()?;
 
     let window = video.window("yasnese v0.1", 256*4, 240*4)
         .position_centered()
         .build()
         .unwrap();
-    let mut canvas = window.into_canvas().accelerated().build().unwrap();
+    let mut canvas = window.into_canvas().accelerated().build()?;
 
     canvas.set_logical_size(256, 240);
 
     let texture_creator = canvas.texture_creator();
 
-    let mut texture = texture_creator.create_texture_streaming(PixelFormatEnum::ARGB8888, 256, 240).unwrap();
+    let mut texture = texture_creator.create_texture_streaming(PixelFormatEnum::ARGB8888, 256, 240)?;
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
 
-    let mut event_pump = sdl_ctx.event_pump().unwrap();
+    let mut event_pump = sdl_ctx.event_pump()?;
     let i = 0;
     let mut pause: bool = false;
 
-    let mut last_frame_time = SystemTime::now();
+    nes.powerup();
+    nes.reset();
+
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -68,7 +68,8 @@ fn main() {
 
         nes.run(&mut texture);
         canvas.clear();
-        canvas.copy(&texture, None, None).unwrap();
+        canvas.copy(&texture, None, None)?;
         canvas.present();
     }
+    Ok(())
 }
